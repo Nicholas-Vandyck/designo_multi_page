fae76c942204ea13d8f98385f96cd896e2b006d7
Nicholas-Vandyck <nicholas.vandyck@amalitech.com>
2023-05-30 16:34:16 +0000

    update

---
+++ starter-code/js/web_design.js
@@ -0,0 +1,5 @@
+const image = [
+    {
+        src: "/starter-code/assets/web-design/desktop/image-express.jpg", alt: "Image 1", text: "EXPRESS", paragraph: "A multi-carrier shipping website for ecommerce businesses"
+    }
+]
