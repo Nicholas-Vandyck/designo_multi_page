ff589dfc5f0d5d94e6236c68409a417346f52f8c
ericmanu <eric.manu@amalitech.com>
2023-06-01 16:38:35 +0000

    worked on the app-section and pages

--- starter-code/js/app-design.js
+++ starter-code/js/app-design.js
@@ -1,45 +1,71 @@
-const gridStuff = [
-    {
-        image: "/starter-code/assets/app-design/desktop/image-airfilter.jpg",
-        name: "Airfilter",
-        use : "Solving the problem of poor indoor air quality by filtering the air"
-    },
-    {
-        image : "/starter-code/assets/app-design/desktop/image-eyecam.jpg",
-        name : "Eyecam",
-        use : "Product that lets you edit your favorite photos and videos at any time"
-    },
-    {
-        image: "/starter-code/assets/app-design/desktop/image-faceit.jpg",
-        name: "Faceit",
-        use: "Get to meet your favorite internet superstar with the faceit app"
-    },
-    {
-        image: "/starter-code/assets/app-design/desktop/image-todo.jpg",
-        name: "Todo",
-        use: "A todo app that features cloud sync with light and dark mode"
-    },
-    {
-        image: "/starter-code/assets/app-design/desktop/image-loopstudios.jpg",
-        name: "Loopstudios",
-        use: "A VR experience app made for Loopstudios"
-    }
+const gridStuffs = [
+  {
+    image: "/starter-code/assets/app-design/desktop/image-airfilter.jpg",
+    name: "Airfilter",
+    use: "Solving the problem of poor indoor air quality by filtering the air",
+  },
+  {
+    image: "/starter-code/assets/app-design/desktop/image-eyecam.jpg",
+    name: "Eyecam",
+    use: "Product that lets you edit your favorite photos and videos at any time",
+  },
+  {
+    image: "/starter-code/assets/app-design/desktop/image-faceit.jpg",
+    name: "Faceit",
+    use: "Get to meet your favorite internet superstar with the faceit app",
+  },
+  {
+    image: "/starter-code/assets/app-design/desktop/image-todo.jpg",
+    name: "Todo",
+    use: "A todo app that features cloud sync with light and dark mode",
+  },
+  {
+    image: "/starter-code/assets/app-design/desktop/image-loopstudios.jpg",
+    name: "Loopstudios",
+    use: "A VR experience app made for Loopstudios",
+  },
 ];
 
+const stuff = document.querySelector(".stuff");
+gridStuffs.forEach(gridstuff => {
+  const box = document.createElement("div");
+  box.classList.add("box");
 
-const box = document.querySelector(".box");
+  const innerBox = document.createElement("div");
+  innerBox.classList.add("innerbox");
+  box.appendChild(innerBox);
 
-const innerBox = document.createElement("div");
-innerBox.classList.add("innerbox");
+  const boxImage = document.createElement("img");
+  boxImage.classList.add("in_img");
+  boxImage.src = gridstuff.image;
+  innerBox.appendChild(boxImage);
 
-const boxImage = document.createElement("div");
-boxImage.classList.add("in_img");
-innerBox.appendChild(boxImage);
+  const words = document.createElement("div");
+  words.classList.add("words");
+  box.appendChild(words);
 
-const words = document.createElement("div");
-words.classList.add("words");
+  const header = document.createElement("h2");
+  header.textContent = gridstuff.name;
+  words.appendChild(header);
 
-console.log(innerBox);
-// for(let i=0; i<gridStuff.length; i++) {
-//     console.log(gridStuff[i]);
-// }
+  const paragraph = document.createElement("p");
+  paragraph.textContent = gridstuff.use;
+  words.appendChild(paragraph);
+
+  stuff.appendChild(box);
+  console.log(box);
+});
+
+const pagesLinks = [
+  {
+    title: "Web Design",
+    description: "View Projects"
+  },
+  {
+    title: "Web Design",
+    description: "View Projects" 
+  }
+]
+
+  
+
