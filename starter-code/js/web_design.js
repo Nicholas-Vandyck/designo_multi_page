f05583be763ac100f661b9c7c01c89ea242118b3
Nicholas-Vandyck <nicholas.vandyck@amalitech.com>
2023-06-08 12:45:22 +0000

    worked on humbager

--- starter-code/js/web_design.js
+++ starter-code/js/web_design.js
@@ -1,21 +1,18 @@
-  // Get the grid container element
-  const gridContainer = document.getElementById('gridContainer');
+const open = document.querySelector('.mob-open');
+const close = document.querySelector('.mob-close');
+const tap = document.querySelector('.link')
 
-  // Create 3 rows and 3 columns
-  for (let row = 1; row <= 3; row++) {
-    for (let col = 1; col <= 3; col++) {
-      // Create a new grid item element
-      const gridItem = document.createElement('div');
-      gridItem.className = 'grid-item';
+open.addEventListener('click',(e) =>{
+  e.preventDefault();
+  tap.style.display = 'block';
+  open.style.display = 'none';
+  close.style.display = 'flex';
+  
+})
 
-      // Create an h1 or h2 element based on the row number
-      const heading = document.createElement(row === 1 ? 'h1' : 'h2');
-      heading.textContent = `Row ${row}, Column ${col}`;
-
-      // Append the heading to the grid item element
-      gridItem.appendChild(heading);
-
-      // Append the grid item to the grid container
-      gridContainer.appendChild(gridItem);
-    }
-  }
+close.addEventListener('click',(e) =>{
+  e.preventDefault();
+  tap.style.display = 'none';
+  open.style.display = 'flex';
+  close.style.display = 'none';
+});
